dc596b798a5e34e7f8baa6c4b7517c08ac1ba56a
furuta <>
2019-10-12 14:00:47 -0400

    [modify] changing endpoint url in url handling task

--- js_server_side/3_url_handling/server/test.js
+++ js_server_side/3_url_handling/server/test.js
@@ -9,20 +9,16 @@ const port = 8080; // Error: listen EACCES: permission denied 127.0.0.1:80
 http
   .createServer(function(req, res) {
     var queryData = url.parse(req.url, true); // parse the URL data
-    console.log(queryData.pathname);
     var filename = "../client" + queryData.pathname; // assign a file name for retrieval
-    if (filename.slice(-1) !== "/") {
-      filename += "/";
-    }
-    filename += "index.html";
     console.log(filename);
 
     // Read the file. If it exists send the contents back, if not return a 404 error
     fs.readFile(filename, function(err, data) {
       if (err) {
+        console.log("Not found:" + filename);
         res.writeHead(404, { "Content-Type": "text/html" });
         return res.end(
-          '<h1>404 Not Found</h1><p>return to <a href="/">default page</a></p>'
+          '<h1>404 Not Found</h1><p>return to <a href="/index.html">default page</a></p>'
         );
       }
       res.writeHead(200, { "Content-Type": "text/html" });
